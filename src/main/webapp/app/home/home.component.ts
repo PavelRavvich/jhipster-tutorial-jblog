84e3188631e2f8815122d9a1b8ec5a380120efe5
Pavel <triodjangopiter@yandex.ru>
2018-02-25 00:50:23 +0300

    add posts list into home page

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { JhiEventManager } from 'ng-jhipster';
-
-import { Account, LoginModalService, Principal } from '../shared';
+import {PostService} from "../entities/post/post.service";
+import {Post} from "../entities/post/post.model";
+import {ResponseWrapper} from "../shared/model/response-wrapper.model";
 
 @Component({
     selector: 'jhi-home',
@@ -13,36 +12,25 @@ import { Account, LoginModalService, Principal } from '../shared';
 
 })
 export class HomeComponent implements OnInit {
-    account: Account;
-    modalRef: NgbModalRef;
 
-    constructor(
-        private principal: Principal,
-        private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
-    ) {
+    posts: Post[] = [];
+
+    constructor(private postService: PostService) {}
+
+    loadAll() {
+        this.postService.query().subscribe(
+            (res: ResponseWrapper) => {
+                console.log(res.json);
+                this.posts = res.json;
+            },
+            (res: ResponseWrapper) => {
+                console.log( res.json );
+            }
+        );
     }
 
     ngOnInit() {
-        this.principal.identity().then((account) => {
-            this.account = account;
-        });
-        this.registerAuthenticationSuccess();
-    }
-
-    registerAuthenticationSuccess() {
-        this.eventManager.subscribe('authenticationSuccess', (message) => {
-            this.principal.identity().then((account) => {
-                this.account = account;
-            });
-        });
-    }
-
-    isAuthenticated() {
-        return this.principal.isAuthenticated();
-    }
-
-    login() {
-        this.modalRef = this.loginModalService.open();
+        console.log('ngOnInit...');
+        this.loadAll();
     }
 }
